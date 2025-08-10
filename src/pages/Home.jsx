ec9b70b4b1859974e4dcb29bebcaa3034204bea8
Alexis <a.eroldan@hotmail.com>
2025-08-10 12:06:27 -0300

    ADD: "Agregado de Layout para manejo de plantillas"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import { Footer } from "../components/Footer"
-import { Header } from "../components/Header"
+
+import { Layout } from "../components/Layout"
 
 const Home = () => {
   return (
     <>
-      <Header/>
-      <h1>Hola Mundo</h1>
-      <Footer/>
+      <Layout>
+        <h1>Hola Mundo</h1>
+      </Layout>
+
     </>
   )
 }
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,22 +1,24 @@
-import { Footer } from "../components/Footer"
-import { Header } from "../components/Header"
+import { Layout } from "../components/Layout"
+
 const PageNotFound = () => {
   return (
-      <>
-      <Header />
-      <div>
-        <h1>🛍️ ¡Uy! Página no encontrada (404)</h1>
-        <p >
-        Parece que esta página se ha perdido entre nuestros productos.
-      </p>
-      <p >
-        Puede que el enlace esté roto o que el producto ya no exista.
-      </p>
-      </div>
-      <Footer/>
-      </>
+    <>
+      <Layout>
+        <div>
+          <h1>🛍️ ¡Uy! Página no encontrada (404)</h1>
+          <p >
+            Parece que esta página se ha perdido entre nuestros productos.
+          </p>
+          <p >
+            Puede que el enlace esté roto o que el producto ya no exista.
+          </p>
+        </div>
+      </Layout>
+
+
+    </>
 
   )
 }
 
-export {PageNotFound}
+export { PageNotFound }
