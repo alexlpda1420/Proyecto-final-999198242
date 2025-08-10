fdce7c383580a4514600fa96cc258a5822e1a112
Alexis <a.eroldan@hotmail.com>
2025-08-10 16:07:18 -0300

    ADD: "Contenido estatico en Register"

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,26 @@ import { Layout } from "../components/Layout/Layout"
 const Register = () => {
   return (
     <Layout>
-      <h2>Register</h2>
+      <h1>Hola desde register</h1>
+      <h1>Registrate</h1>
+
+      <section>
+        <form>
+          <div>
+            <label>Username:</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Correo electrónico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }
