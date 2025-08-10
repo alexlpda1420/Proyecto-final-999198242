7fc9f8a776d06e808812609b4238e39941a665ec
Alexis <a.eroldan@hotmail.com>
2025-08-10 19:25:40 -0300

    ADD: "Incorporacion de peticiones fetch. get y delete"

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -7,15 +7,22 @@ const Header = () => {
     <header>
       <img src="https://images.seeklogo.com/logo-png/43/2/escudo-river-plate-2022-logo-png_seeklogo-435015.png" alt="Imagen-Logo" />
       <nav>
-        <ul>
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/Dashboard">Dashboard</Link></li>
-          <li><Link to="/AboutUs">Sobre Nosotros</Link></li>
-          <li><Link to="/Login">Login</Link></li>
-          <li><Link to="/Register">Registro</Link></li>
-          <button>Cerrar Sesión</button>
+        <ul>     
+          
           {
-            user &&  <li><Link to="/admin">Admin</Link></li>
+            user &&
+            <>
+              <li><Link to="/">Inicio</Link></li>
+              <li><Link to="/Dashboard">Dashboard</Link></li>
+              <li><Link to="/AboutUs">Sobre Nosotros</Link></li>
+              <button>Cerrar Sesión</button>
+            </>
+          }
+          {
+            !user && <>
+            <li><Link to="/Login">Login</Link></li>
+            <li><Link to="/Register">Registrate</Link></li>
+            </>
           }
          
         </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
 
 import { Layout } from "../components/Layout/Layout"
-
+import {useEffect, useState } from "react"
 const Home = () => {
+  const [products, setProducts] = useState([])
+  const [user, setUser] = useState(true)
+  
+
+  const fetchingProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products", { method: "GET" })
+    const data = await response.json()
+    setProducts(data)
+  }
+
+  useEffect(() => { 
+    fetchingProducts()
+  }, [])
+
+  const handleDelete = async (id) => {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, { method: "DELETE" })
+
+    if (response.ok) {
+      setProducts(prevProduct => prevProduct.filter((product) => product.id != id))
+    }
+
+
+
+
+  }
+
+
   return (
     <>
       <Layout>
@@ -32,7 +59,23 @@ const Home = () => {
         <h2>Nuestros productos</h2>
           <p>Elegí entre nuestras categorías más populares.</p>
           <div>
-            <p>Cargando productos...</p>
+            { 
+              products.map((product) => <div key={product.id}>
+                <h2 key={product.id}>{product.title}</h2>
+                <img width="80px" src={product.image} alt={`Imagen de ${product.title}`} />
+                <p>${product.price}</p>
+                <p>{product.description}</p>
+                <p><strong>{product.category}</strong></p>
+                {
+                  user &&  <div>
+                  <button>Actualizar</button>
+                    <button onClick={() => handleDelete(product.id) }>Borrar</button>
+                </div>
+                
+               }
+                
+              </div>)
+            }
           </div>
      
           </section>
