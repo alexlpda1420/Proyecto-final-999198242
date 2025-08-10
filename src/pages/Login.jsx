561e43fdf86c24542bc84462abe52ff5092ec16e
Alexis <a.eroldan@hotmail.com>
2025-08-10 16:05:33 -0300

    ADD: "Contenido estatico en Login"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,22 @@ import { Layout } from "../components/Layout/Layout"
 const Login = () => {
   return (
     <Layout>
-      <h2>Login</h2>
+       <h1>Hola desde login</h1>
+      <h1>Inicia sesión</h1>
+
+      <section>
+        <form>
+          <div>
+            <label>Correo electrónico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }
