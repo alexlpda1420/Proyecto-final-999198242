861b60c92e5d319e23e3535ef1d95b0840939423
Alexis <a.eroldan@hotmail.com>
2025-08-10 16:02:37 -0300

    ADD: "Contenido estatico en Dashboard"

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,29 @@ import { Layout } from "../components/Layout/Layout"
 const Dashboard = () => {
   return (
     <Layout>
-      <h2>Dashboard</h2>
+      <h1>Panel de Administracion</h1>
+
+      <section>
+        <h2>Cargar nuevo producto</h2>
+        <form>
+          <div>
+            <label>Nombre del Producto: </label>
+            <input type="text" name="name" />
+          </div>
+
+          <div>
+            <label>Precio: </label>
+            <input type="number" name="price" />
+          </div>
+
+          <div>
+            <label>Descripcion: </label>
+            <textarea name="description" rows="4"></textarea>
+          </div>
+
+          <button>Guardar Producto</button>
+        </form>
+      </section>
     </Layout>
   )
 }
