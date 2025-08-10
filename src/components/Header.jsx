628294e004159e6f0880c9abc2bc88b779aa5ce6
Alexis <a.eroldan@hotmail.com>
2025-08-10 14:17:39 -0300

    ADD: "Agregado de estructura base de las paginas"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 const Header = () => {
+  const [user, setUser] = useState(true)
   return (
   
     <header>
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Register">Registro</Link></li>
+          <li><Link to="/Login">Login</Link></li>
+          <li><Link to="/Dashboard">Dashboard</Link></li>
+          {
+            user &&  <li><Link to="/admin">Admin</Link></li>
+          }
+         
         </ul>
       </nav>
     </header>
--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "../pages/Home"
 import { PageNotFound } from "../pages/PageNotFound"
+import { Register } from "../pages/Register"
+import { Login } from "../pages/Login"
+import { Dashboard } from "../pages/Dashboard"
 const RouterApp = () => {
   return (
     
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Register" element={<Register />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Dashboard" element={<Dashboard/>}></Route>
         <Route path="*" element={ <PageNotFound/>} />
       </Routes>
     
